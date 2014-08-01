6edb8e7630eee7d8b86ff092f35f1824360a0b7b
Paul Sore <paul.sore@gmail.com>
2014-08-01 20:10:55 +0200

    Ajout page "Services" / css
    + meilleur usage de la grid bootstrap
    - drede font
    + bree font
    + plugin textfield
    + textfields sur "services"

--- footer.php
+++ footer.php
@@ -4,16 +4,18 @@
  *
  * Contains footer content and the closing of the #main and #page div elements.
  *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
  */
 ?>
 
 </div><!-- #main -->
 
-<footer id="colophon" class="site-footer" role="contentinfo">
-  <b>TODO FOOTER</b>
+<footer id="footer" class="site-footer" role="contentinfo">
+
+  <form id="contact" action="" method="post">
+      <h1 class="white">Contact</h1>
+      <h2>Et si on en discutait ?</h2>
+  </form>
+
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
---
+++ page-about.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Template Name: About
+ */
+
+get_header(); ?>
+
+  <div id="main-content" class="main-content about container-fluid">
+
+    <?php
+    if ( is_front_page() ) {
+      // Include the featured content template.
+      get_template_part( 'featured-content' );
+    }
+    ?>
+    <div id="primary" class="content-area row">
+      <div id="content" class="site-content col-lg-11 col-lg-offset-1" role="main">
+
+        <?php while ( have_posts() ) : the_post(); ?>
+
+          <div class="row">
+            <div class="col-lg-9">
+              <?php the_content() ?>
+            </div>
+          </div>
+
+          <div class="row">
+
+            <?php
+
+              # Iterer sur les champs présents dans le gorupe "Extra text" de la page
+              # Voir -> http://simple-fields.com/documentation/getting-started/tutorial-part-2/
+              #
+              foreach(simple_fields_get_post_group_values(get_the_id(), "Extra text", false, 1)[1] as $i=>$textcontent) {
+
+                  // icones au dessus de chaque post
+                  $icons = array(
+                    "flaticon-responsive", // bloc intégration html/php/resp
+                    "flaticon-like3", // bloc intégration sociale
+                    "flaticon-stats" // bloc intégration SEO
+                  );
+
+                  echo '<div class="col-lg-4 item">';
+                    echo '<i class="icon '. $icons[$i] .'"></i>';
+                    echo '<div class="text">'. $textcontent .'</div>';
+                  echo '</div>';
+
+              }
+
+            ?>
+
+          </div>
+
+        <?php endwhile; ?>
+
+      </div><!-- #content -->
+    </div><!-- #primary -->
+    <?php get_sidebar( 'content' ); ?>
+  </div><!-- #main-content -->
+
+<?php
+get_sidebar();
+get_footer();
